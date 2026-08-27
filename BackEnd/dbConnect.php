2ae23b826644340f5b4d7d74f6d24758ef86f9c6
Asif Ahmed <135535126+ShafiAhmed10@users.noreply.github.com>
2026-08-27 09:54:51 +0600

    Branch Commit

--- BackEnd/dbConnect.php
+++ BackEnd/dbConnect.php
@@ -10,9 +10,5 @@ $conn = new mysqli($host, $username, $password, $database);
 if ($conn->connect_error) {
     die("Database connection failed: " . $conn->connect_error);
 }
-else
-{
-	echo "connection successful";
-}
 $conn->set_charset("utf8mb4");
 ?>
